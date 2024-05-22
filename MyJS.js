4b8c82b256fc0301e648795b77d745addec1ed8d
rovingwind <rovingwind93@gmail.com>
2024-05-22 16:00:36 +0800

    update

--- MyJS.js
+++ MyJS.js
@@ -7,44 +7,44 @@ MyJS = {
     const level = pm.globals.get("script_level")
     console.log(`${dividerChar.repeat(120)} [${level}]_${scriptType} ${scriptState}`);
   },
-  
+
   SetEnvironmentLevel: (num) => {
     const levels = {
       0: "collection",
       1: "folder",
       2: "request"
     };
-    
+
     pm.globals.set("script_level", levels[num]);
   },
-  
+
   Exit: () => pm.globals.unset("script_level"),
-  
+
   GetVariablesAndCheck: (layer, key) => {
     const layerMapping = {
       [VariablesLayer.Globals]: pm.globals,
       [VariablesLayer.CollectionVariables]: pm.collectionVariables,
       [VariablesLayer.Environment]: pm.environment
     };
-  
+
     const valueSource = layerMapping[layer];
     if (!valueSource) {
       throw new Error(`未知的 ${layer} 層`);
     }
-  
+
     let value = valueSource.get(key);
-    
+
     if (typeof value === "undefined") {
       throw new Error(`${layer} 層 ${key} 尚未設定值`);
     }
-    
+
     if (typeof value === "string") {
       value = value.trim();
     }
-    
+
     return value;
   },
-  
+
   // 依類型取得變數
   getV: (type, key, isBool = false) => {
     const typeString = {
@@ -53,17 +53,17 @@ MyJS = {
       [pm.environment]: "pm.environment"
     };
     const typeName = typeString[type] || JSON.stringify(type);
-    
+
     if(!type.has(key)) {
       throw new Error(`${typeName} 尚未設定變數 ${key}`);
     }
-    
+
     let value = type.get(key)
-    
+
     if (typeof value === 'string') {
-      value = value.trim(); 
+      value = value.trim();
     }
-    
+
     if (isBool) {
       if (value === '0') return false;
       if (value === '1') return true;
@@ -71,26 +71,26 @@ MyJS = {
 
     return value;
   },
-  
+
   logPreTest: async (type, name, eventName, action) => {
     const emojiMapping = {
       'Request': '😀',
       'Folder': '📁',
       'Collection': '📦',
     };
-    
+
     const emojiType = emojiMapping[type] || '';
     const emojiStart = '🟢';
     const emojiEnd = '🔴';
     const layerNamePad = 20;
     const layer = emojiType + " " + name.padEnd(layerNamePad, " ");
     const progress = `${layer} => ${pm.info.eventName.padEnd(10, " ")}`;
-  
+
     console.log(`${progress} ${emojiStart}`);
-    
+
     // 有傳入 function 的話執行
     if (action && typeof action === 'function') await action();
-  
+
     console.log(`${progress} ${emojiEnd}`);
   },
 
@@ -98,7 +98,7 @@ MyJS = {
     if (!(Array.isArray(data))) {
       throw new Error("data 不是 Array");
     }
-    
+
     let keys = data[0];
     var arrayObj = data.slice(1).map(item => {
       let obj = {};
@@ -109,13 +109,13 @@ MyJS = {
 
       return obj;
     });
-    
+
     return arrayObj;
   },
-  
-  sendRequest: (req) => {
+
+  sendRequest: async (req) => {
     return new Promise((resolve, reject) => {
-      
+
       pm.sendRequest(req, (err, res) => {
         if (err) {
           return reject(err);
@@ -140,6 +140,6 @@ function GetArrayObj(data) {
     }
 
     return obj;
-  });	
+  });
   return list;
 };
